bf7ca5d74348b826b2bab2532fdf07a33b892e16
Maksim Chemerisuk <chemerisuk@gmail.com>
2016-04-06 19:52:58 +0300

    1.0.0-beta.3

--- dist/better-time-element.js
+++ dist/better-time-element.js
@@ -1,85 +1,139 @@
 /**
- * better-time-element: Various <time> element extensions
- * @version 1.0.0-beta.2 Sat, 23 Jan 2016 12:33:34 GMT
+ * better-time-element: Useful <time> element extensions
+ * @version 1.0.0-beta.3 Wed, 06 Apr 2016 16:52:58 GMT
  * @link https://github.com/chemerisuk/better-time-element
  * @copyright 2016 Maksim Chemerisuk
  * @license MIT
  */
-(function(DOM, DateUtils) {
+(function (DOM, DateUtils) {
     "use strict";
 
     var __ = DOM.__;
-    var pad = function(num, maxlen)  {return ((maxlen === 2 ? "0" : "00") + num).slice(-maxlen)};
+    var pad = function (num, maxlen) {
+        return maxlen === 1 ? num : ("00" + num).slice(-maxlen);
+    };
 
     DOM.extend("time[is=local-time]", {
-        constructor: function() {
+        constructor: function () {
             this.on("change", this._changeValue.bind(this));
 
             this._changeValue();
         },
-        _changeValue: function() {
-            var value = new Date(this.get("datetime")),
+        _changeValue: function () {
+            var datetimeText = this.get("datetime");
+
+            if (!datetimeText) return;
+
+            var value = new Date(datetimeText),
                 formattedValue = "";
 
             if (!value.getTime()) {
                 formattedValue = value.toString();
             } else {
                 var formatString = this.get("data-format");
-                // use "E, dd MMM yyyy" as default value
-                if (!formatString) formatString = "E, dd MMM yyyy";
+                // use "E, dd MMM yyyy H:mm:ss" as default value
+                if (!formatString) formatString = "E, dd MMM yyyy H:mm:ss";
 
-                var day = value.getUTCDay();
-                var date = value.getUTCDate();
-                var month = value.getUTCMonth();
-                var year = value.getUTCFullYear();
-                var hour = value.getHours();
-                var hourAMPM = hour % 12 || 12;
-                var minutes = value.getMinutes();
-                var seconds = value.getSeconds();
+                formattedValue = formatString.replace(/\w+/g, function (str) {
+                    switch (str) {
+                        case "H":
+                        case "HH":
+                            str = pad(value.getHours(), str.length);
+                            break;
 
-                formatString = formatString
-                        .replace(/'([^']+)'/g, "->$1<-")
-                        .replace(/\w+/g, "{$&}")
-                        .replace(/->{(.*?)}<-/g, function(_, group)  {return group.replace(/}|{/g, "")});
+                        case "h":
+                        case "hh":
+                            str = pad(value.getHours() % 12 || 12, str.length);
+                            break;
 
-                formattedValue = DOM.format(formatString, {
-                    H: hour,
-                    HH: pad(hour, 2),
-                    h: hourAMPM,
-                    hh: pad(hourAMPM, 2),
-                    m: minutes,
-                    mm: pad(minutes, 2),
-                    s: seconds,
-                    ss: pad(seconds, 2),
-                    E: __(DateUtils.DAYS[day].slice(0, 2)).toHTMLString(),
-                    EE: __(DateUtils.DAYS[day]).toHTMLString(),
-                    d: date,
-                    dd: pad(date, 2),
-                    D: DateUtils.getDayInYear(value),
-                    DD: pad(DateUtils.getDayInYear(value), 3),
-                    w: DateUtils.getWeekInYear(value),
-                    ww: pad(DateUtils.getWeekInYear(value), 2),
-                    W: DateUtils.getWeekInMonth(value),
-                    M: month + 1,
-                    MM: pad(month + 1, 2),
-                    MMM: __(DateUtils.MONTHS[month].substr(0, 3) + ".").toHTMLString(),
-                    MMMM: __(DateUtils.MONTHS[month]).toHTMLString(),
-                    y: year % 100,
-                    yy: pad(year % 100, 2),
-                    yyyy: year,
-                    u: day || 7,
-                    F: DateUtils.getWeekCountInMonth(value)
+                        case "m":
+                        case "mm":
+                            str = pad(value.getMinutes(), str.length);
+                            break;
+
+                        case "s":
+                        case "ss":
+                            str = pad(value.getSeconds(), str.length);
+                            break;
+
+                        case "d":
+                        case "dd":
+                            str = pad(value.getDate(), str.length);
+                            break;
+
+                        case "E":
+                            str = __(DateUtils.DAYS[value.getDay()].slice(0, 2));
+                            break;
+
+                        case "EE":
+                            str = __(DateUtils.DAYS[value.getDay()]);
+                            break;
+
+                        case "D":
+                        case "DD":
+                            str = pad(DateUtils.getDayInYear(value), str.length === 1 ? 1 : 3);
+                            break;
+
+                        case "w":
+                        case "ww":
+                            str = pad(DateUtils.getWeekInYear(value), str.length);
+                            break;
+
+                        case "W":
+                            str = DateUtils.getWeekInMonth(value);
+                            break;
+
+                        case "M":
+                        case "MM":
+                            str = pad(value.getMonth() + 1, str.length);
+                            break;
+
+                        case "MMM":
+                            str = __(DateUtils.MONTHS[value.getMonth()].substr(0, 3) + ".");
+                            break;
+
+                        case "MMMM":
+                            str = __(DateUtils.MONTHS[value.getMonth()]);
+                            break;
+
+                        case "y":
+                        case "yy":
+                            str = pad(value.getFullYear() % 100, str.length);
+                            break;
+
+                        case "yyyy":
+                            str = value.getFullYear();
+                            break;
+
+                        case "u":
+                            str = value.getDay() || 7;
+                            break;
+
+                        case "F":
+                            str = DateUtils.getWeekCountInMonth(value);
+                            break;
+                    }
+
+                    return str.toString();
                 });
             }
 
-            // display formatted date value instead of real one
             this.value(formattedValue);
         }
     });
-}(window.DOM, {
+
+    // compact months in english don't have the dot suffix
+    DOM.importStrings("en", DateUtils.MONTHS.reduce(function (memo, month) {
+        var shortMonth = month.slice(0, 3);
+
+        memo[shortMonth + "."] = shortMonth;
+
+        return memo;
+    }, {}));
+})(window.DOM, {
     DAYS: "Sunday Monday Tuesday Wednesday Thursday Friday Saturday".split(" "),
     MONTHS: "January February March April May June July August September October November December".split(" "),
-    getWeekInYear: function(d) {
+    getWeekInYear: function (d) {
         d = new Date(Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate()));
         // set to nearest thursday: current date + 4 - current day number
         // make sunday's day number 7
@@ -88,17 +142,17 @@
         // calculate full weeks to nearest thursday
         return Math.ceil((1 + (d - yearStart) / 86400000) / 7);
     },
-    getWeekInMonth: function(d) {
+    getWeekInMonth: function (d) {
         var firstWeekday = new Date(Date.UTC(d.getUTCFullYear(), d.getUTCMonth(), 1)).getUTCDay();
         var offsetDate = d.getUTCDate() + firstWeekday - 1;
         return 1 + Math.floor(offsetDate / 7);
     },
-    getWeekCountInMonth: function(d) {
+    getWeekCountInMonth: function (d) {
         return Math.ceil(d.getUTCDate() / 7);
     },
-    getDayInYear: function(d) {
+    getDayInYear: function (d) {
         var beginOfYear = Date.UTC(d.getUTCFullYear(), 0, 1);
         var millisBetween = d.getTime() - beginOfYear;
         return Math.floor(1 + millisBetween / 86400000);
     }
-}));
+});
